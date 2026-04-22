bcd89fed791ce24186371f45d98086e9f5c02e6f
local.brawit <phoksosatsyabrawit@gmail.com>
2026-04-22 14:46:11 +0700

    test commit

--- Student.java
+++ Student.java
@@ -3,4 +3,35 @@ public class Student {
     private String name;
     private String gender;
     private int age;
+
+    // Setter
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setGender(String gender){
+        if("male".equalsIgnoreCase(gender) || "m".equalsIgnoreCase(gender)){
+            this.gender = gender;
+        }
+        if("female".equalsIgnoreCase(gender) || "f".equalsIgnoreCase(gender)){
+            this.gender = gender;
+        }
+        this.gender = null;
+    }
+    public void setAge(int age){
+        if(age > 0){
+            this.age = age;
+        }
+    }
+
+    // Getter
+    public String getName(){
+        return name;
+    }
+    public String getGender(){
+        return gender;
+    }
+    public int getAge(){
+        return age;
+    }
 }
+
