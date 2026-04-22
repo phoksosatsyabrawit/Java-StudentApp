92150a5f76558d3b4f48d33515c77d729b8899fe
local.brawit <phoksosatsyabrawit@gmail.com>
2026-04-22 19:56:01 +0700

    update studentservice

--- StudentService.java
+++ StudentService.java
@@ -1,5 +1,23 @@
 package studPrac01;
+import java.util.Scanner;
 
 public class StudentService {
 
+    public Student createStudent(){
+        Scanner scan = new Scanner(System.in);
+        Student students = new Student();
+        System.out.print("Name: ");
+        students.setName(scan.next());
+        System.out.print("Gender: ");
+        students.setGender(scan.next());
+        System.out.print("Age: ");
+        students.setAge(scan.nextInt());
+
+        return students;
+    }
+
+    public void print(Student students){
+        System.out.println("================== Student List ==================");
+        System.out.println("Name: " + students.getName() + "; Gender: " + students.getGender() + "; Age: " + students.getAge());
+    }
 }
