08e449bf8d68a516e57b47900368b730251fd3bc
local.brawit <phoksosatsyabrawit@gmail.com>
2026-04-24 14:52:54 +0700

    update feature

--- StudentApp.java
+++ StudentApp.java
@@ -1,4 +1,5 @@
 package studPrac01;
+import java.util.Arrays;
 
 public class StudentApp {
     public static void main(String[] args){
@@ -6,5 +7,11 @@ public class StudentApp {
         int numberofStud = 2;
         Student[] students = studentservice.createStudentArr(numberofStud);
         studentservice.print(students);
+        Student[] sortbyName = studentservice.sortbyName(students);
+        studentservice.printsortbyName(sortbyName);
+        Student[] sortbyAge = studentservice.sortbyAge(students);
+        studentservice.printsortbyAge(sortbyAge);
+        Student catchOldestStud = studentservice.catchOldestStud(students);
+        studentservice.printcatchOldestStud(catchOldestStud);
     }
 }
--- StudentService.java
+++ StudentService.java
@@ -21,7 +21,6 @@ public class StudentService {
         System.out.println("================== Student List ==================");
         for(int i=0; i<students.length; i++){
             System.out.println("Name: " + students[i].getName() + "; Gender: " + students[i].getGender() + "; Age: " + students[i].getAge());
-            System.out.println("\n");
         }
     }
 
@@ -32,5 +31,64 @@ public class StudentService {
             students[i] = studArr.createStudent();
         }
         return students;
+    }
+
+    // bubble sort algorithm
+    public Student[] sortbyName(Student[] students){
+        for(int i=0; i<students.length; i++){
+            for(int j=i+1; j<students.length; j++){
+                if(students[i].getName().compareTo(students[j].getName()) > 0){
+                    Student temp = new Student();
+                    temp = students[i];
+                    students[i] = students[j];
+                    students[j] = temp;
+                }
+            }
+        }
+        return students;
+    }
+
+    public void printsortbyName(Student[] students){
+        System.out.println("\n#Sorting by Name.");
+        for(int i=0; i<students.length; i++){
+            System.out.println("Name: " + students[i].getName());
+        }
+    }
+
+    // bubble sort algorithm
+    public Student[] sortbyAge(Student[] students){
+        for(int i=0; i<students.length; i++){
+            for(int j=i+1; j<students.length; j++){
+                if(students[i].getAge() > students[j].getAge()){
+                    Student temp = new Student();
+                    temp = students[i];
+                    students[i] = students[j];
+                    students[j] = temp;
+                }
+            }
+        }
+        return students;
+    }
+
+    public void printsortbyAge(Student[] students){
+        System.out.println("\n#Sorting by Age.");
+        for(int i=0; i<students.length; i++){
+            System.out.println("Age: " + students[i].getAge());
+        }
+    }
+
+    public Student catchOldestStud(Student[] students){
+        Student maxAge = students[0];
+        for(int i=0; i<students.length; i++){
+            if(maxAge.getAge() < students[i].getAge()){
+                maxAge = students[i];
+            }
+        }
+        return maxAge;
+    }
+
+    public void printcatchOldestStud(Student students){
+        System.out.println("\n#Catch Oldest Student.");
+        System.out.println("Name: " + students.getName() + "; Age: " + students.getAge());
     }
 }
